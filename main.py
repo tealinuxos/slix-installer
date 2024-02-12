209f175cb1a9fc11500e45c5622657f6158704cc
0x86 <51450260+x0rr-dan@users.noreply.github.com>
2024-02-12 07:02:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -424,11 +424,11 @@ def set_user_pass(username, user_pass):
 ## need upgrade to add more desktop environment options ##
 def install_desktop_environment(desktop_env):
     if desktop_env.lower() == "gnome":
-        subprocess.run(["arch-chroot /mnt pacman -S --noconfirm gnome gnome-tweaks gdm xorg-server xorg-apps xorg-xinit xterm pipewire pipewire-pulse && systemctl enable gdm -f"], shell=True, check=True) ## error in systemctl start gdm idk in arch chroot got error maybe to the another desktop enviroment is the same one, try to remove systemctl enable gdm start first and run again in pain, bro my internet is really slow that why this take so long
+        subprocess.run(["arch-chroot /mnt pacman -S --noconfirm gnome gnome-tweaks gdm xorg-server xorg-apps xorg-xinit xterm pipewire pipewire-pulse && arch-chroot /mnt systemctl enable gdm -f"], shell=True, check=True) ## error in systemctl start gdm idk in arch chroot got error maybe to the another desktop enviroment is the same one, try to remove systemctl enable gdm start first and run again in pain, bro my internet is really slow that why this take so long
     elif desktop_env.lower() == "xfce":
-        subprocess.run(["arch-chroot /mnt pacman -S --noconfirm xfce4 sddm xfce4-goodies xorg-server xorg-apps xorg-xinit xterm pipewire pipewire-pulse && systemctl enable sddm -f"], shell=True, check=True)
+        subprocess.run(["arch-chroot /mnt pacman -S --noconfirm xfce4 sddm xfce4-goodies xorg-server xorg-apps xorg-xinit xterm pipewire pipewire-pulse && arch-chroot /mnt systemctl enable sddm -f"], shell=True, check=True)
     elif desktop_env.lower() == "kde":
-        subprocess.run(["arch-chroot /mnt pacman -S --no-confirm plasma kdm konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 xorg-server xorg-apps xorg-xinit xterm pipewire pipewire-pulse && systemctl enable kdm -f"], check=True)
+        subprocess.run(["arch-chroot /mnt pacman -S --no-confirm plasma kdm konsole dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5 xorg-server xorg-apps xorg-xinit xterm pipewire pipewire-pulse && arch-chroot /mnt systemctl enable kdm -f"], check=True)
     else:
         print(f"{co.ye}Desktop environment '{desktop_env}' is not supported for now.{co.re}")
 
@@ -594,7 +594,7 @@ def teaprompt():
                 if keymap in keymap_list:
                     print(f"{co.g}[*] Keymap set to {selected_keymap}{co.re}")
                 else:
-                    print(f"{co.y}[!] Invalid keymap code. Please select a valid keymap.{co.re}")
+                    print(f"{co.ye}[!] Invalid keymap code. Please select a valid keymap.{co.re}")
             else:
                 print(f"[!] Invalid {co.r}'set keymap'{co.re} command format. Please use {co.g}'set keymap keymap_code'{co.re}.")
             
