74d46736e73aae3de92bf0de7a1ef996a4d360dd
0x86 <51450260+x0rr-dan@users.noreply.github.com>
2024-10-05 09:45:11 +0700

    fix bug
    fix bug that i forget about it T_T

--- core/root_pass.py
+++ core/root_pass.py
@@ -3,8 +3,8 @@ import subprocess
 
 ### 1.8 set root pass if user input it ###
 def set_root_pass(pass_root):
-    passwd_input = f"{pass_root}\n{pass_root}\n"
-    result = subprocess.run(['arch-chroot', '/mnt', 'passwd'], input=passwd_input.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    passwd_input = f"{pass_root}"
+    result = subprocess.run(['arch-chroot', '/mnt', 'export', f'$pass="{pass_root}"', '&&', 'echo', '$pass', '|', 'passwd', '--stdin'], input=passwd_input, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if result.returncode == 0:
         print(f"{co.g}[*] Root password has been set.{co.re}")
     else:
