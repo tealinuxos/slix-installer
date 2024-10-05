375b8416eb916645a41a29ba6b35ad971523045f
0x86 <51450260+x0rr-dan@users.noreply.github.com>
2024-10-05 10:05:11 +0700

    Update root_pass.py

--- core/root_pass.py
+++ core/root_pass.py
@@ -3,9 +3,11 @@ import subprocess
 
 ### 1.8 set root pass if user input it ###
 def set_root_pass(pass_root):
-    passwd_input = f"{pass_root}"
-    result = subprocess.run(['arch-chroot', '/mnt', 'export', f'$pass="{pass_root}"', '&&', 'echo', '$pass', '|', 'passwd', '--stdin'], input=passwd_input, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    if result.returncode == 0:
-        print(f"{co.g}[*] Root password has been set.{co.re}")
-    else:
-        print(f"{co.r}[!] Error setting root password: {result.stderr}{co.re}")
+    try:
+        result = subprocess.run(f'echo -e "{pass_root}\n{pass_root}" | passwd root', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode == 0:
+            print(f"{co.bo}{co.g}[*] Root password has been set.{co.re}")
+        else:
+            print(f"{co.r}{co.bo}[!] Error setting root password: {result.stderr}{co.re}")
+    except Exception as e:
+        print(f"[!] Exception occurred: {e}")
